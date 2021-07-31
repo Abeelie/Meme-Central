68d629a1ae0f41a65ab7ab181112356937d9851f
Abeelie <abrahaelill@gmail.com>
2021-07-31 13:00:17 -0400

    added image

--- static/js/stocks/news.js
+++ static/js/stocks/news.js
@@ -11,7 +11,7 @@ try {
   <div class="card is-horizontal">
     <div class="card-image">
       <figure class="image is-square">
-        <img class="imgg" src=${article.image} style="object-fit:cover; width:300px;
+        <img src=${article.image} style="object-fit:cover; width:300px;
                                   height:100;" alt="image">
       </figure>
     </div>
@@ -32,15 +32,6 @@ try {
 
   $("#news").append($news);
 });
-
-  const imgError = document.querySelector(".imgg");
-
-  if (response.data.articles.image) {
-    imgError.src = response.data.articles.image;
-  }else {
-    imgError.src = "https://vcunited.club/wp-content/uploads/2020/01/No-image-available-2.jpg"
-  }
-
 
 
 } catch(error){
